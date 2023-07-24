5814255f4ad3ee4f62f2bf3b1cf165fc9cbc8605
pablo.perez <pablo.perez@MacBook-Air-de-Pablo.local>
2023-07-24 09:43:21 +0200

    Simplifiying code 1.0

--- src/components/molecules/LuminosityDisplay.tsx
+++ src/components/molecules/LuminosityDisplay.tsx
@@ -1,34 +1,39 @@
 import React, { useContext } from "react";
 import { DataContext } from "../../App";
-import dayIcon from "../../assets/day.png"; // Replace with your actual file nam
-import twilightIcon from "../../assets/twilight.png"; // Replace with your actual file name
-import nightIcon from "../../assets/night.png"; // Replace with your actual file name
+import dayIcon from "../../assets/day.png";
+import twilightIcon from "../../assets/twilight.png";
+import nightIcon from "../../assets/night.png";
+
+type LuminosityLevelType = {
+  [key: string]: { min?: number; max?: number; icon: string };
+};
+
+// Define a mapping object to handle the luminosity levels
+const luminosityLevels: LuminosityLevelType = {
+  Día: { max: 5, icon: dayIcon },
+  Crepusculo: { min: 6, max: 10, icon: twilightIcon },
+  Noche: { min: 11, icon: nightIcon },
+};
 
 const LuminosityDisplay: React.FC = () => {
-  const context = useContext(DataContext);
-
-  if (!context) {
-    return <div>Cargando...</div>;
-  }
-
-  const { luminosityData } = context;
+  const { luminosityData } = useContext(DataContext) || {};
 
   if (!luminosityData) {
     return <div>Cargando...</div>;
   }
 
-  let luminosityLevel;
-  let icon;
-  if (luminosityData.value <= 5) {
-    luminosityLevel = "Día";
-    icon = dayIcon;
-  } else if (luminosityData.value <= 10) {
-    luminosityLevel = "Crepusculo";
-    icon = twilightIcon;
-  } else {
-    luminosityLevel = "Noche";
-    icon = nightIcon;
+  const { value } = luminosityData;
+
+  // Find the correct luminosity level based on the value
+  const [luminosityLevel, levelData] = Object.entries(luminosityLevels).find(
+    ([_, { min = -Infinity, max = Infinity }]) => value >= min && value <= max
+  ) || [null, {}];
+
+  if (!luminosityLevel) {
+    return <div>Invalid luminosity value: {value}</div>;
   }
+
+  const { icon } = levelData;
 
   return (
     <div className="h-[36vh] bg-slate-100 rounded-lg px-6 w-[25vw] py-2 shadow-md">
@@ -39,15 +44,13 @@ const LuminosityDisplay: React.FC = () => {
           <span className="font-bold">{luminosityLevel}</span>
         </p>
         <p className="text-xl">
-          Valor de Visibilidad:{" "}
-          <span className="font-bold">{luminosityData.value}</span>
+          Valor de Visibilidad: <span className="font-bold">{value}</span>
         </p>
         <img
           src={icon}
           alt={luminosityLevel}
           className="w-36 pt-5 self-center"
-        />{" "}
-        {/* Replace with desired width and height */}
+        />
       </div>
     </div>
   );
